ed0db7265f6d4b25ac4ed70886751a19fe8619c2
root <root@DESKTOP-DRJPTAC.localdomain>
2020-08-13 10:38:58 +0900

    inistial

--- src/main.js
+++ src/main.js
@@ -12,7 +12,17 @@ Vue.config.productionTip = false
 Vue.config.debug = true
 
 axios.defaults.baseURL = process.env.VUE_APP_URL
-firebase.initializeApp(process.env.VUE_APP_FIREBASE_CONFIG)
+//firebase.initializeApp(process.env.VUE_APP_FIREBASE_CONFIG)
+var firebaseConfig = {
+  apiKey: "AIzaSyBCtkiqT0oEBhTUf0myLAda9TFnAInET1o",
+  authDomain: "dice-adab6.firebaseapp.com",
+  databaseURL: "https://dice-adab6.firebaseio.com",
+  projectId: "dice-adab6",
+  storageBucket: "dice-adab6.appspot.com",
+  messagingSenderId: "178770782401",
+  appId: "1:178770782401:web:f760e833ea5e0343f20c40"
+};
+firebase.initializeApp(firebaseConfig)
 
 new Vue({
   router,
