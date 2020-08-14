465799a81f28b5d1ab94e7c7a121c1bc7a690d08
root <root@DESKTOP-DRJPTAC.localdomain>
2020-08-14 20:58:11 +0900

    inistial

--- src/App.vue
+++ src/App.vue
@@ -245,7 +245,8 @@
         const provider = new firebase.auth.TwitterAuthProvider()
         firebase.auth().signInWithPopup(provider)
         if(this.twitter_user.uid)
-          this.login = true,alert('TW LOGIN')
+          this.$set(this.login, true)
+
       },
       doLogout() {
         this.login = false,this.entry= false
