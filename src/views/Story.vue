0702863f66ace02a9d6e2317fd872d5e7de3bfa6
root <root@DESKTOP-DRJPTAC.localdomain>
2020-08-16 22:10:46 +0900

    inistial

--- src/views/Story.vue
+++ src/views/Story.vue
@@ -153,12 +153,13 @@ export default {
       await this.$store.commit('notifyTrpgSessionImg',this.sceneData[0]['scene_image'])
       await this.$store.commit('notifyTrpgSessionBgm',this.sceneData[0]['scene_bgm'])
       await this.$store.commit('notifySessionSceneId',this.sceneSelect.session_scene_id)
+      this.doStoryFireBaseUpdate()
   },
   doStoryFireBaseUpdate() {
     firebase.database().ref('message').push({
     message: 'storyUpdate|'+this.$store.getters.sessionSceneId
     }, () => {
-      this.textarea_dice_command = ""
+
     })
   }      
   
