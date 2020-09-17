99607955ba951ab822f4aa65fc7f1d14f4ed78dd
root <root@DESKTOP-DRJPTAC.localdomain>
2020-09-17 17:43:04 +0900

    inistial

--- src/mixins/App/ticketMixin.js
+++ src/mixins/App/ticketMixin.js
@@ -24,6 +24,7 @@ export default  {
           twUID:this.$store.getters.twUID,
           updateDate:date.getTime()
         }
+        
       );
     },
     async ticketFireBaseStateUpdateTicektRelease() {
