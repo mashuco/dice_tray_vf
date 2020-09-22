ab9b4657e7f3fd01774843ec597f5684b6b30ce2
root <root@DESKTOP-DRJPTAC.localdomain>
2020-09-23 01:56:55 +0900

    inistial

--- src/mixins/App/firebaseMixin.js
+++ src/mixins/App/firebaseMixin.js
@@ -93,7 +93,7 @@ export default  {
         //if(snapshot.val()!=null)
           //this.loginUsers=snapshot.val()
           console.log("snapshot")
-          console.log(snapshot.val)
+          console.log(snapshot)
     
       })
     },
