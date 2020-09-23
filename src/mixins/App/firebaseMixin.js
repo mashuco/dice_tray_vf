f008232617d1065f923f0505cd275e5fbe545e17
root <root@DESKTOP-DRJPTAC.localdomain>
2020-09-23 12:06:50 +0900

    inistial

--- src/mixins/App/firebaseMixin.js
+++ src/mixins/App/firebaseMixin.js
@@ -84,12 +84,12 @@ export default  {
     },
     fireBaseLiveUpdateLoginUsers(){
       console.log('/login/'+this.$store.getters.trpgSessionId)
+      let list = [];
       var fbRef = firebase.database().ref('/login/'+this.$store.getters.trpgSessionId+'/')
       fbRef.on("value", (data)=> {
         if (data) {
             const rootList = data.val();
             const key = data.key;
-            let list = [];
             // データオブジェクトを配列に変更する
             if(rootList != null) {
                 Object.keys(rootList).forEach((val, key) => {
