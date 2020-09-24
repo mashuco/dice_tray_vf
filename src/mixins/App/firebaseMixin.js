16e03bacf65ec9afb88f61e7a82da496327a8b01
root <root@DESKTOP-DRJPTAC.localdomain>
2020-09-24 16:14:38 +0900

    inistial

--- src/mixins/App/firebaseMixin.js
+++ src/mixins/App/firebaseMixin.js
@@ -167,6 +167,11 @@ export default  {
 
       if(snap.val().ticketId==this.$store.getters.ticketId) {
         this.dialogMsgArr =[]
+        this.dialogMsgArr.push(" snap.val().twUID::::"+ snap.val().twUID)
+        this.dialogMsgArr.push("this.$store.getters.twUID:::"+this.$store.getters.twUID)
+        this.dialogMsgArr.push("snap.val().ticketId:::"+snap.val().ticketId)
+        this.dialogMsgArr.push("this.$store.getters.ticketId::::"+this.$store.getters.ticketId)
+        
         this.dialogMsgArr.push("このチケットは他ユーザーに取得されました")
         this.dialogMsgArr.push("強制ログアウトします")
         this.dialogLogout = true
