32b23c84e8684dca1590dec6f1ae0eb30c514ff4
root <root@DESKTOP-DRJPTAC.localdomain>
2020-10-02 11:38:34 +0900

    inistial

--- src/services/axsiosServ.js
+++ src/services/axsiosServ.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export default  {
   axsiosInitial(twLinkedAuthKey){
-    axios.defaults.headers.common['Authorization'] = "Bearer " + twLinkedAuthKey
+    axios.defaults.headers.common['Authorization'] =`Bearer ${twLinkedAuthKey}`
     Vue.prototype.$axios = axios.create({ 
       baseURL:  process.env.VUE_APP_URL,
       auth: {
