1e1570292bc48cddcfaf8cd3a2c6996cd51b733f
root <root@DESKTOP-DRJPTAC.localdomain>
2020-10-02 16:09:04 +0900

    inistial

--- src/services/axsiosServ.js
+++ src/services/axsiosServ.js
@@ -6,10 +6,10 @@ export default  {
     //axios.defaults.headers.common['Authorization'] = "Token " + twLinkedAuthKey
     Vue.prototype.$axios = axios.create({ 
       baseURL:  process.env.VUE_APP_URL,
-      auth: {
-        username: process.env.VUE_APP_BASIC_USER ,
-        password: process.env.VUE_APP_BASIC_PASS
-      }, 
+      //auth: {
+      //  username: process.env.VUE_APP_BASIC_USER ,
+      //  password: process.env.VUE_APP_BASIC_PASS
+      //}, 
       //headers: {
       //  "Content-Type": "application/json",
      // }
