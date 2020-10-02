58bf0b6a93ecffc37fca8bc93493fe71908216a4
root <root@DESKTOP-DRJPTAC.localdomain>
2020-10-02 15:53:45 +0900

    inistial

--- src/App.vue
+++ src/App.vue
@@ -321,9 +321,13 @@
          this.loadAllSession()
       },
       async loadAllSession(){
+        console.log("loaddAll!")
+        console.log(this.$store.getters.twLinkedAuthKey)
+
           await this.$axios.get('/session/?format=json',
 {
     headers: {
+     "Content-Type": "application/json",
       authorization: "Token: " + this.$store.getters.twLinkedAuthKey
     }
   }          
